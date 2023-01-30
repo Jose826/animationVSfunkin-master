800f4f25ec5b4006522b3b1480d50bd93e3a09e3
Jose826 <98564980+Jose826@users.noreply.github.com>
2023-01-30 13:12:05 -0300

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -19,6 +19,7 @@ import sys.FileSystem;
 #end
 import flixel.graphics.FlxGraphic;
 import openfl.display.BitmapData;
+import lime.system.System as Shit;
 
 import flash.media.Sound;
 
@@ -200,8 +201,18 @@ class Paths
 
 	static public function video(key:String)
 	{
-		
-		return 'assets/videos/$key';
+		#if MODS_ALLOWED
+		var file:String = Shit.applicationStorageDirectory + modsVideo(key);
+		if(FileSystem.exists(file)) {
+			return file;
+		}
+		#end
+		return Shit.applicationStorageDirectory + 'assets/videos/$key.$VIDEO_EXT';
+	}
+
+        static public function _video(key:String)
+	{
+		return 'assets/videos/$key.$VIDEO_EXT';
 	}
 
 	static public function sound(key:String, ?library:String):Sound
