445e072c8fdecf840e26e09a0b2ab94414e36438
Jose826 <98564980+Jose826@users.noreply.github.com>
2023-01-30 17:43:36 -0300

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -199,22 +199,10 @@ class Paths
 		return getPath('$key.lua', TEXT, library);
         }
 
-	static public function video(key:String)
-	{
-		#if MODS_ALLOWED
-		var file:String = Shit.applicationStorageDirectory + modsVideo(key);
-		if(FileSystem.exists(file)) {
-			return file;
-		}
-		#end
-		return Shit.applicationStorageDirectory + 'assets/videos/$key.$VIDEO_EXT';
-	}
-
-        static public function _video(key:String)
-	{
-		return 'assets/videos/$key.$VIDEO_EXT';
-	}
-
+	inline static public function video(key:String)
+        {
+	return 'assets/videos/$key';
+        }
 	static public function sound(key:String, ?library:String):Sound
 	{
 		var sound:Sound = returnSound('sounds', key, library);
